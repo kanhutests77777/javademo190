f2e3abe77634a9c7da6f4240df018ed1308ce7a3
kanhu69 <kanhu.barik77@gmail.com>
2020-04-21 17:29:39 +0530

    1

---
+++ Demo1/src/test/java/demo1/Demo1/Hellooworld.java
@@ -0,0 +1,11 @@
+package demo1.Demo1;
+
+public class Hellooworld {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+          System.out.println("hello world");
+
+}
+}
